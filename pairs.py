8307c992d517e2d90179845f1f738f34e9aa5c0f
sak <sakher.x@gmail.com>
2023-05-08 21:25:26 -0400

    fx

--- pairs.py
+++ pairs.py
@@ -1,16 +1,14 @@
-def pairs(k, arr):
+# takes O(nlgn + lgn)
+def pairs_slower(k, arr):
     # python's sort takes O(nlogn)
     arr.sort()
 
     def binary_search(lst, item):
-        if len(lst) == 1 and lst[0] == item:
-            return True
-        elif len(lst) == 1 and lst[0] != item:
-            return False
-
         mid = int(len(lst) / 2)
         if item == lst[mid]:
             return True
+        elif item != lst[mid] and len(lst) == 1:
+            return False
         elif item < lst[mid]:
             return binary_search(lst[:mid], item)
         elif item > lst[mid]:
@@ -27,4 +25,19 @@ def pairs(k, arr):
     
     return counter
 
-print(pairs(2, [1,2,3,4,5]))
+
+def pairs(k, arr):
+    dict = {}
+    counter = 0
+    for item in arr:
+        dict[item] = 1
+        if (item + k) in dict :
+            counter += 1
+            dict[(item + k)] = 1
+        elif (item - k) in dict :
+            counter += 1
+            dict[(item - k)] = 1
+    
+    return counter
+
+print(pairs(2, [1,5,3,4,2]))
